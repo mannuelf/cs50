6d7bf1189f3e64fcbf8841b13e2e5fa85a3ecfe6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-05-19 08:59:04 +0200

    greedy example

--- pset1/greedy/greedy.c
+++ pset1/greedy/greedy.c
@@ -4,12 +4,40 @@
 
 int main(void)
 {
-    float n;
+    float amount_given;
+    
+    int QUARTER = 25;
+    int DIMES = 10;
+    int NICKEL = 5;
+    int PENNIES = 1;
+    int balance;
+    int coins;
+
     do
     {
-        n = get_float();
+        printf("Thank you for the: $ ");
+        amount_given = get_float();
     }
-    while (n > 0);
+    while (amount_given < 0);
+    
+    // round input to cents
+    balance = (int)(amount_given * 100.0);
+
+    // while quarters can be used
+    coins += balance / QUARTER; // increase amount
+    balance %= QUARTER; // amount decreased by a QUARTER
+     
+    // while dimes can be used
+    coins += balance / DIMES; // increase amount
+    balance %= DIMES; // amount decrease by a dime
+    
+    // while pennies can used
+    coins += balance / PENNIES; // increase amount
+    balance %= PENNIES; // amount decrease by a penny
+    
+    // print number of coins used
+    printf("Numbers of coins needed: %i\n", coins);
+    printf("Your change is: %i\n", balance);
 }
 
 
